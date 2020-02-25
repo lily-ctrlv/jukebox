b7fe561f92f6b24909fe2fa1bae84bed8f52ca85
Liliana Martins <lilianasrmartins@gmail.com>
2020-02-25 20:36:59 +0000

    finished ui kit and uploaded screenshot

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_20_210958) do
-
+ActiveRecord::Schema.define(version: 2020_02_22_161615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -51,7 +49,6 @@ ActiveRecord::Schema.define(version: 2020_02_20_210958) do
   create_table "event_tracks", force: :cascade do |t|
     t.bigint "track_id"
     t.bigint "event_id"
-
     t.integer "total_bid_amount"
     t.integer "rank"
     t.datetime "created_at", null: false
