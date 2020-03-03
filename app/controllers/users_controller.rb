0654fb9e4db2660d944469626700c19a21188c43
James Trinder <jamesanthonyt@live.co.uk>
2020-03-03 21:21:49 +0000

    updated the bottom nav a little and added a log out but that doesnt work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    @user = current_user
     @recent_10_bids = Bid.where(user_id: @user.id).order(created_at: :desc).first(10)
   end
 end
