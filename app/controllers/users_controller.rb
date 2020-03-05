de9eba42673f000e16ec7cd85af36735451be38d
James Trinder <jamesanthonyt@live.co.uk>
2020-03-05 19:04:43 +0000

    thought this went through earlier

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @user = current_user
-    @recent_10_bids = Bid.where(user_id: @user.id).order(created_at: :desc).first(10)
+    redirect_to root_path unless current_user
+    @recent_10_bids = Bid.where(user_id: current_user.id).order(created_at: :desc).first(10)
   end
 end
