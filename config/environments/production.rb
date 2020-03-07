33329aefde4cc30c8cb3623442f43528a9d4715c
Eric Burger <eric.p.burger@gmail.com>
2020-03-07 22:07:44 +0000

    reinstated production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
 end
