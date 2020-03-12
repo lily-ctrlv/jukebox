df6ebf8474d85a9c432bf7fb1bfbc7cdb467646c
Eric Burger <eric.p.burger@gmail.com>
2020-03-12 20:44:50 +0000

    updated metatags

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -1,14 +1,14 @@
 module MetaTagsHelper
   def meta_title
-    content_for?(:meta_title) ? content_for(:meta_title) : DEFAULT_META["JukeBox - Control Your Clubbing Beats"]
+    content_for?(:meta_title) ? content_for(:meta_title) : DEFAULT_META["meta_title"]
   end
 
   def meta_description
-    content_for?(:meta_description) ? content_for(:meta_description) : DEFAULT_META["Jukebox is a mobile application which enables clubbers to control the night's playlist."]
+    content_for?(:meta_description) ? content_for(:meta_description) : DEFAULT_META["meta_description"]
   end
 
   def meta_image
-    meta_image = (content_for?(:meta_image) ? content_for(:meta_image) : DEFAULT_META["jukebox-icon.png"])
+    meta_image = (content_for?(:meta_image) ? content_for(:meta_image) : DEFAULT_META["meta_image"])
     # little twist to make it work equally with an asset or a url
       # meta_image.starts_with?("http") ? meta_image : image_url(meta_image)
   end
