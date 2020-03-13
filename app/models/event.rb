11f69387a641a993c510fa759fd3cc2abfec01ca
James Trinder <jamesanthonyt@live.co.uk>
2020-03-13 11:06:10 +0000

    added dj front end tasks

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,6 @@ class Event < ApplicationRecord
   has_one_attached :event_image
   has_many :event_tracks
 
-  validates :theme, presence: true
   validates :name, presence: true
   validates :start_date_time, presence: true
   validates :end_date_time, presence: true
